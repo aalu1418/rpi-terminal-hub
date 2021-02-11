0ee16195164ea21fa1a7f0f3050569cb0e447055
aalu1418 <aalu1418@gmail.com>
2021-02-11 13:12:03 -0500

    white text for rpi

--- weather.py
+++ weather.py
@@ -12,8 +12,15 @@ class Weather():
         res = requests.get(f"https://wttr.in/{self.location}")
         # print(json.dumps(res.text))
         self.data = res.text.replace("\n\nFollow \033[46m\033[30m@igor_chubin\033[0m for wttr.in updates\n", "")
-        self.data = self.data.replace("\u001b[38;5;240;1m", "\u001b[0;0;0;1m") #replace dark grey clouds with white
-        self.data = self.data.replace("\u001b[38;5;250m", "\u001b[0;0;0;1m") #replace grey clouds with white
+
+        #color replacement
+        white = "\u001b[0;0;0;1m"
+        self.data = self.data.replace("\u001b[38;5;240;1m", white) #replace dark grey clouds with white
+        self.data = self.data.replace("\u001b[38;5;250m", white) #replace grey clouds with white
+        self.data = white + self.data #show white text
+        self.data = self.data.replace("\u001b[0m", "\u001b[0m"+white) #show white text
+
+        #arrow replacement with directions
         self.timestamp = time.strftime("%b %d @ %I:%M %p")
 
 if __name__ == '__main__':
