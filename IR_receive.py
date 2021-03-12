ad1fda4a69d28776325710140ad18dcf0132702b
aalu1418 <aalu1418@gmail.com>
2021-03-12 16:47:50 -0500

    printing values

--- IR_receive.py
+++ IR_receive.py
@@ -3,10 +3,13 @@
 from gpiozero import LineSensor
 from signal import pause
 
-sensor = LineSensor(17)
-sensor.when_line = lambda: print('Line detected')
-sensor.when_no_line = lambda: print('No line detected')
-pause()
+sensor = LineSensor(17, sample_rate=2000)
+# sensor.when_line = lambda: print('Line detected')
+# sensor.when_no_line = lambda: print('No line detected')
+# pause()
+
+while True:
+    print(sensor.value)
 
 
 
