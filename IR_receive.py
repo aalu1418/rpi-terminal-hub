0091bf12c0399da39309f2099f9b2304e1fba41d
aalu1418 <aalu1418@gmail.com>
2021-03-12 16:55:09 -0500

    trying to capture signal

--- IR_receive.py
+++ IR_receive.py
@@ -8,8 +8,13 @@ sensor = LineSensor(17, sample_rate=2000)
 # sensor.when_no_line = lambda: print('No line detected')
 # pause()
 
-while True:
-    print(sensor.value)
+data = []
+sensor.wait_for_line()
+
+for i in range(2000):
+    data.append(sensor.value)
+
+print(sum(data), len(data))
 
 
 
