4c652cb60dd2d19e1138a78a10c8e4180df57a07
aalu1418 <aalu1418@gmail.com>
2021-03-13 08:59:33 -0500

    reformatted recieve to class based

--- IR_receive.py
+++ IR_receive.py
@@ -1,39 +1,61 @@
 # example: https://fishandwhistle.net/post/2016/raspberry-pi-pure-python-infrared-remote-control/
 
 from gpiozero import LineSensor
-import time
+from time import time
+import json
+import sys
 
-sensor = LineSensor(17, sample_rate=38000, queue_len=1)
-# sensor.when_line = lambda: print('Line detected')
-# sensor.when_no_line = lambda: print('No line detected')
-# pause()
+class Receiver():
+    def __init__(self, GPIO):
+        self.GPIO = GPIO
+        self.data = []
+        self.parsed = {0: [], 1:[]}
 
-data = []
-sensor.wait_for_line()
+        self.sensor = LineSensor(self.GPIO, sample_rate=38000, queue_len=1)
 
-start = time.time()
-for i in range(15000):
-    data.append(sensor.value)
+    # record + parse
+    def run(self, n=15000):
+        self.sensor.wait_for_line()
 
-duration = time.time()-start
-print("Total duration: ", duration, "\n")
+        start = time()
+        for i in range(n):
+            self.data.append(self.sensor.value)
 
-value = 0
-parsed = {0: [], 1:[]}
+        self.duration = time()-start
 
-# sorting data into struct
-count = 0
-for v in data:
-    if v == value:
-        count += 1
-    else:
-        parsed[value].append(count*duration/len(data))
+        # sorting data into struct
+        value = 0
         count = 0
-        value = int(not(value))
+        for v in self.data:
+            if v == value:
+                count += 1
+            else:
+                self.parsed[value].append(count*self.duration/len(self.data))
+                count = 0
+                value = int(not(value))
 
-# add last count to data struct
-parsed[value].append(count*duration/len(data))
+        # add last count to data struct
+        self.parsed[value].append(count*self.duration/len(self.data))
 
-for i in range(len(parsed[0])):
-    print("On: ", parsed[0][i])
-    print("Off: ", parsed[1][i], "\n")
+    # export data
+    def print(self, export=False):
+        print("Total duration: ", self.duration, "\n")
+
+        for i in range(len(self.parsed[0])):
+            print("On: ", self.parsed[0][i])
+            print("Off: ", self.parsed[1][i], "\n")
+
+        if export is True:
+            file = open("./data/IR_receive.json", "w")
+            data = json.dumps(self.parsed)
+            file.writelines(data)
+            file.close()
+
+if __name__ == '__main__':
+    export = False
+    if "--export" in sys.argv:
+        export = True
+
+    receiver = Receiver(17)
+    receiver.run()
+    receiver.print(export)
