bf741c5aca6f606b295e83337f736bcc358080b9
aalu1418 <aalu1418@gmail.com>
2021-04-22 18:57:15 -0400

    fix git cloning repo

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y git python3-pip pigpio vim
 pip3 install pigpio ircodec flask requests python-dotenv
 
 # pull repo
-git pull https://github.com/aalu1418/rpi-terminal-hub
+git clone https://github.com/aalu1418/rpi-terminal-hub
 
 # setup cron job
 crontab -l | { cat; echo "@reboot sleep 60 && /usr/bin/python3 /home/pi/rpi-terminal-hub/main.py --server >> ~/cron.log 2>&1"; } | crontab -
