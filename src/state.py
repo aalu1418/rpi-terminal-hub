bfb14a15c0a2fde4a5b7eaed4ec033ad8793a1a0
aalu1418 <aalu1418@gmail.com>
2021-05-28 07:03:10 -0400

    state update bug fixes

--- src/state.py
+++ src/state.py
@@ -8,10 +8,26 @@ class State:
         if not hasattr(self, key): # if new key, set
             setattr(self, key, data)
             self.changed = True
+            return
 
-        self.changed = json.dumps(data) == json.dumps(getattr(self, key)) # check if existing value matchess
+        self.changed = json.dumps(data) != json.dumps(getattr(self, key)) # check if existing value matchess
         if self.changed: # if check does not match, update
             setattr(self, key, data)
 
     def clear(self):
         self.changed = False
+
+if __name__ == '__main__':
+    def printVar():
+        print('Update:', test.changed, test.__dict__)
+
+    test = State()
+    printVar()
+    test.update('something', 1)
+    printVar()
+    test.clear()
+    printVar()
+    test.update('something', 1)
+    printVar()
+    test.update('something', 2)
+    printVar()
